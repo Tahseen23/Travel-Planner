420fcf2abc135887dcc41db13c16bd11f2b65aeb
Tahseen Kausar <tahseenkausar24@gmail.com>
2025-01-22 23:28:37 +0530

    Basic front end part completed

--- client/vite-project/src/pages/Home.jsx
+++ client/vite-project/src/pages/Home.jsx
@@ -1,14 +1,25 @@
 import './HomeCss.css'
 import log from '../images/log.jpg'
 import { useNavigate } from "react-router"
+import { useState } from 'react';
+import { OrbitProgress } from "react-loading-indicators"
+
+
+import { FaArrowRight } from "react-icons/fa";
+
 
 const Home = () => {
-  const naviagte=useNavigate()
+  const naviagte = useNavigate()
+  const [loading, setloading] = useState(false)
 
-  const onClick=()=>{
+  const onClick = () => {
     naviagte('/')
-
   }
+
+  const handleButton = () => {
+    setloading(true)
+  }
+
   return (
     <div>
       <div className='header'>
@@ -19,11 +30,22 @@ const Home = () => {
         <div className='heading'>
           <h1>Where You Want to Travel?</h1>
         </div>
+        <div className='input'>
+          <input type="text" placeholder='From' />
+          <input type="text" placeholder='To' />
+          <input type="date" />
+          <div>{
+            !loading ?
+              <button onClick={handleButton} className='arrow'> <FaArrowRight  /></button>
+
+              :
+              <OrbitProgress color="#32cd32" size="small" text="" textColor="" />
+          }</div>
+        </div>
       </div>
-
-
     </div>
   )
-
 }
+
+
 export default Home
